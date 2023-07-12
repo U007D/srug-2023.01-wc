1f7aa3e473e9b91591bfd069093050b9c61fa316
Brad Gibson <u007d@humanenginuity.com>
2023-07-12 08:46:36 -0700

    code compete with `thiserror`

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,21 @@
-use std::fmt::{Display, Formatter};
-
 use {clap::error::ErrorKind as ClapErrorKind, clap::Error as ClapError};
+use thiserror::Error;
 
 use crate::Error::InternalError;
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("Bad argument error: {0}")]
     BadArgument(String),
+    #[error("Word count overflowed `usize` (max: {} words)", usize::MAX)]
     CountOverflow,
+    #[error("Internal error: Unexpected error encountered: {0}")]
     InternalError(String),
-    IoError(std::io::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
 
 impl From<ClapError> for Error {
@@ -25,26 +28,3 @@ impl From<ClapError> for Error {
         }
     }
 }
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
-        Self::IoError(err)
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        use Error::*;
-
-        write!(
-            f,
-            "{}",
-            match self {
-                BadArgument(s) => format!("Bad argument {s}"),
-                _ => "...".into(),
-            }
-        )
-    }
-}
-
-impl std::error::Error for Error {}
