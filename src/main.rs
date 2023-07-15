b427061c76aaf05b9ad66b5555d852453596caa2
Brad Gibson <u007d@humanenginuity.com>
2023-07-15 05:37:38 -0700

    add filename to output

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ use crate::args::Args;
 
 fn inner_main() -> Result<()> {
     let arg = Args::try_parse()?;
-    let source = File::open(arg.source_file)?;
+    let source = File::open(&arg.source_file)?;
     let word_count = count_words(BufReader::new(source))?;
 
-    println!("The file contained {word_count} words.");
+    let filename = arg.source_file.to_string_lossy();
+    println!("The file `{filename}` contains {word_count} words.");
 
     Ok(())
 }
